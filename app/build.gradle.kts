154e4eb2ff41928db46ea10e3418c81105ec0659
Turan Yağcı <tyagci024@gmail.com>
2024-03-16 01:37:20 +0300

    dependecy updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id ("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -33,10 +36,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation ("com.squareup.retrofit2:retrofit:'2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:'2.9.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:'2.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
