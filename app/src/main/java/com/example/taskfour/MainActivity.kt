63f1eb29f2dc0a63e0171764285b6b6bcec2b154
Turan Yağcı <tyagci024@gmail.com>
2024-03-18 19:19:04 +0300

    bottomnavbar eklendi

--- app/src/main/java/com/example/taskfour/MainActivity.kt
+++ app/src/main/java/com/example/taskfour/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.taskfour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.taskfour.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNW = binding.bottomNavigationView
+        setupWithNavController(bottomNW, navController = navController)
+        setupActionBarWithNavController(navController)
     }
 }
