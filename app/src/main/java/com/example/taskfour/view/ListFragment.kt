ba40f01441d33483c9b06712d63aeed66faab0bc
Turan Yağcı <tyagci024@gmail.com>
2024-03-18 13:46:37 +0300

    viewmodel liveddatalarını gözlemledik

--- app/src/main/java/com/example/taskfour/view/ListFragment.kt
+++ app/src/main/java/com/example/taskfour/view/ListFragment.kt
@@ -1,49 +1,68 @@
 package com.example.taskfour.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.example.taskfour.R
-import com.example.taskfour.model.CryptoModel
-import com.example.taskfour.service.CryptoApi
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.taskfour.adapter.Adapter
+import com.example.taskfour.databinding.FragmentListBinding
 import com.example.taskfour.viewModel.ListViewModel
-
 
 class ListFragment : Fragment() {
     private val viewModel: ListViewModel by viewModels()
-    private val cryptoList = MutableLiveData<List<CryptoModel>>()
+    private lateinit var adapterCoin: Adapter
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-
-
-
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.recyclerViewCrypto.layoutManager = LinearLayoutManager(requireContext())
+        liveDataObserver()
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // CryptoListLiveData'nın gözlemlenmesi
-        viewModel.cryptoListObs.observe(viewLifecycleOwner, Observer { cryptoList ->
-            // LiveData'nın değeri değiştiğinde çalışacak kod
-            cryptoList?.let {
-                // Verileri loga yazdırma
-                for (crypto in it) {
-                    Log.d("CryptoData", "${crypto.name}: ${crypto.currentPrice}")
+    }
+
+    fun liveDataObserver(){
+        viewModel.cryptoListObs.observe(viewLifecycleOwner) {
+            it?.let {
+                adapterCoin = Adapter(it)
+                binding.recyclerViewCrypto.adapter = adapterCoin
+                adapterCoin.onItemClickListener = { cryptoModel ->
+                    val action = ListFragmentDirections.actionListFragmentToDetailFragment(cryptoModel)
+                    findNavController().navigate(action)
                 }
             }
-        })
+        }
+        viewModel.loadingObs.observe(viewLifecycleOwner) {
+            if (it) {
+                with(binding){
+                    progressBar.visibility = View.VISIBLE
+                    recyclerViewCrypto.visibility = View.GONE
+                    textviewError.visibility = View.GONE
+                }
+            }
+            else {
+                with(binding){
+                    progressBar.visibility = View.GONE
+                    recyclerViewCrypto.visibility = View.VISIBLE
+                    textviewError.visibility = View.GONE
+                }
+            }
+        }
     }
 }
