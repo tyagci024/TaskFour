364ee6b88317a267234cd2a79d1f09988e82a516
Turan Yağcı <tyagci024@gmail.com>
2024-03-18 13:48:51 +0300

    adapter sınıfını en sade haline getirdim

---
+++ app/src/main/java/com/example/taskfour/adapter/Adapter.kt
@@ -0,0 +1,39 @@
+package com.example.taskfour.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskfour.databinding.ItemLayoutBinding
+import com.example.taskfour.model.CryptoModel
+import com.example.taskfour.utilies.downloadFromURL
+
+class Adapter(private val cryptoList: List<CryptoModel>) :
+    RecyclerView.Adapter<Adapter.CryptoListViewHolder>() {
+    var onItemClickListener: ((CryptoModel) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CryptoListViewHolder {
+        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CryptoListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CryptoListViewHolder, position: Int) {
+        val crypto = cryptoList[position]
+        holder.bind(crypto)
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.invoke(crypto)
+        }
+    }
+
+    override fun getItemCount() = cryptoList.size
+
+    class CryptoListViewHolder(private val binding: ItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(coin: CryptoModel) {
+            binding.apply {
+                nameid.text = coin.name
+                currencyid.text = coin.currentPrice.toString()
+                imageviewCoin.downloadFromURL(coin.image)
+            }
+        }
+    }
+}
