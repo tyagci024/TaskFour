6596a7adcf1da7d9db6bd38d96e444df3aa9305a
Turan Yağcı <tyagci024@gmail.com>
2024-03-18 13:48:04 +0300

    parcelable yaptım taşımak için detail sayfasına

--- app/src/main/java/com/example/taskfour/model/CryptoModel.kt
+++ app/src/main/java/com/example/taskfour/model/CryptoModel.kt
@@ -1,10 +1,16 @@
 package com.example.taskfour.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class CryptoModel(
     @SerializedName("id") val id: String,
     @SerializedName("symbol") val symbol: String,
     @SerializedName("name") val name: String,
     @SerializedName("image") val image: String,
-    @SerializedName("current_price") val currentPrice: Double)
+    @SerializedName("high_24h") val high24h: Double,
+    @SerializedName("low_24h") val low24h: Double,
+    @SerializedName("last_updated") val lastUpdated: String,
+    @SerializedName("current_price") val currentPrice: Double) : Parcelable
