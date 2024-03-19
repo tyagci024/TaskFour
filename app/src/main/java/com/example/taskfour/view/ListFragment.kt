816e2fe62645a61ca97b8f08a405d279ac9d5189
Turan Yağcı <tyagci024@gmail.com>
2024-03-19 10:45:19 +0300

    searchbar eklendi

--- app/src/main/java/com/example/taskfour/view/ListFragment.kt
+++ app/src/main/java/com/example/taskfour/view/ListFragment.kt
@@ -1,6 +1,8 @@
 package com.example.taskfour.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +13,15 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskfour.adapter.Adapter
 import com.example.taskfour.databinding.FragmentListBinding
+import com.example.taskfour.model.CryptoModel
 import com.example.taskfour.viewModel.ListViewModel
+import java.util.Locale
 
 class ListFragment : Fragment() {
     private val viewModel: ListViewModel by viewModels()
     private lateinit var adapterCoin: Adapter
     private lateinit var binding: FragmentListBinding
-
+    private lateinit var originalList: List<CryptoModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -35,11 +39,30 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val searchText = s.toString().toLowerCase(Locale.getDefault())
+                if (::originalList.isInitialized) {
+                    val filteredList = originalList.filter {
+                        it.name.toLowerCase(Locale.getDefault()).contains(searchText)
+                    }
+                    adapterCoin.updateList(filteredList)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
     }
 
     fun liveDataObserver(){
         viewModel.cryptoListObs.observe(viewLifecycleOwner) {
             it?.let {
+                originalList = it
                 adapterCoin = Adapter(it)
                 binding.recyclerViewCrypto.adapter = adapterCoin
                 adapterCoin.onItemClickListener = { cryptoModel ->
