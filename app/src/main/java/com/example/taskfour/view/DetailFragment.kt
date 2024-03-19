2e3fdd679eef6458a274e9d3877116535ef2ba16
Turan Yağcı <tyagci024@gmail.com>
2024-03-19 15:49:17 +0300

    databinding kaldırdım

--- app/src/main/java/com/example/taskfour/view/DetailFragment.kt
+++ app/src/main/java/com/example/taskfour/view/DetailFragment.kt
@@ -1,20 +1,25 @@
 package com.example.taskfour.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.example.taskfour.BR
 import com.example.taskfour.R
 import com.example.taskfour.databinding.FragmentDetailBinding
 import com.example.taskfour.utilies.downloadFromURL
+import com.example.taskfour.viewModel.ListViewModel
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
     private val args by navArgs<DetailFragmentArgs>()
+    private val viewModel: ListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +27,21 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
-        binding.setVariable(BR.Coin,args.currentCoin)
-        binding.executePendingBindings()
-
-
+        with(binding){
+            textViewCoinName.text=args.currentCoin.name
+            textViewCoinSymbol.text=args.currentCoin.symbol
+            textViewPrice.text=args.currentCoin.currentPrice.toString()
+            textViewHigh24h.text=args.currentCoin.high24h.toString()
+        }
+        binding.imageViewFavIcon.setOnClickListener {
+            viewModel.insertCrypto(args.currentCoin)
+            viewModel.readAllData.observe(viewLifecycleOwner, Observer { cryptos ->
+                // Log the items stored in the database
+                cryptos.forEach { crypto ->
+                    Log.d("CryptoItem", "ID: ${crypto.id}, Name: ${crypto.name}, Symbol: ${crypto.symbol}")
+                }
+            })
+        }
         binding.imageViewCoin.downloadFromURL(args.currentCoin.image)
 
         return binding.root
