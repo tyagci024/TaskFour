bccb6db66cf6692ef14917a6d5243800306e1d6b
Turan Yağcı <tyagci024@gmail.com>
2024-03-21 18:02:58 +0300

    hardcoded edit

--- app/src/main/java/com/example/taskfour/service/CryptoApiService.kt
+++ app/src/main/java/com/example/taskfour/service/CryptoApiService.kt
@@ -5,7 +5,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class CryptoApiService {
-    private val BASE_URL = "https://api.coingecko.com/api/v3/"
+    companion object {
+        const val URL = "https://api.coingecko.com/api/v3/"
+    }
+
+    private val BASE_URL = URL
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
