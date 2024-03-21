83e6fb0f4a622b13a0946ee1ef6f5458f691374f
Turan Yağcı <tyagci024@gmail.com>
2024-03-21 15:16:42 +0300

    coinin database kontrol sorgusu ekledim

--- app/src/main/java/com/example/taskfour/room/CryptoRepository.kt
+++ app/src/main/java/com/example/taskfour/room/CryptoRepository.kt
@@ -8,14 +8,21 @@ class CryptoRepository(private val cryptoDao: CryptoDao) {
     suspend fun insertCrypto(crypto: CryptoModel) {
         cryptoDao.insertCrypto(crypto)
     }
-    suspend fun getCryptoBySymbol(symbol: String): CryptoModel? {
-        return cryptoDao.getCryptoBySymbol(symbol)
-    }
     suspend fun updateCrypto(crypto: CryptoModel) {
         cryptoDao.updateCrypto(crypto)
     }
+    fun getCryptoBySymbol(symbol: String): LiveData<CryptoModel?> {
+        return cryptoDao.getCryptoBySymbol(symbol)
+    }
+
     suspend fun deleteBySymbol(symbol: String){
         cryptoDao.deleteCryptoBySymbol(symbol)
     }
+    fun isSymbolInDatabase(symbol: String):LiveData<Boolean>{
+        return cryptoDao.isSymbolInDatabase(symbol)
+    }
 
+    suspend fun getAllCrypto(): List<CryptoModel> {
+        return cryptoDao.getAllCrypto()
+    }
 }
