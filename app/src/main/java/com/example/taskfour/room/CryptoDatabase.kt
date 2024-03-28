59127f464c48f8e252b2fdc67c8c15f6f6b70bce
Turan Yağcı <tyagci024@gmail.com>
2024-03-28 16:40:29 +0300

    room set

--- app/src/main/java/com/example/taskfour/room/CryptoDatabase.kt
+++ app/src/main/java/com/example/taskfour/room/CryptoDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.taskfour.model.CryptoModel
 
-@Database(entities = [CryptoModel::class], version = 4, exportSchema = false)
+@Database(entities = [CryptoModel::class], version = 5, exportSchema = false)
 abstract class CryptoDatabase : RoomDatabase() {
     abstract fun cryptoDao(): CryptoDao
 
