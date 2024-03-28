9a8a21629acb3346cbd6b525d374c9c917a5a959
Turan Yağcı <tyagci024@gmail.com>
2024-03-28 16:38:39 +0300

    detail ekranı set

--- app/src/main/java/com/example/taskfour/view/DetailFragment.kt
+++ app/src/main/java/com/example/taskfour/view/DetailFragment.kt
@@ -1,19 +1,16 @@
 package com.example.taskfour.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.example.taskfour.R
 import com.example.taskfour.databinding.FragmentDetailBinding
 import com.example.taskfour.utilies.downloadFromURL
 import com.example.taskfour.viewModel.DetailViewModel
-import com.example.taskfour.viewModel.ListViewModel
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
