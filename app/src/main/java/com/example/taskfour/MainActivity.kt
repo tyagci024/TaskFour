62453a3d9cdee6b3265f1d22b4fe9e748e225f0e
Turan Yağcı <tyagci024@gmail.com>
2024-04-04 13:06:00 +0300

    di implement

--- app/src/main/java/com/example/taskfour/MainActivity.kt
+++ app/src/main/java/com/example/taskfour/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.taskfour.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
