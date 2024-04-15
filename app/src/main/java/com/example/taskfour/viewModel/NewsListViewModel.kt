aadf4040272aca28f5a586bf32266bf7cd60e33d
Turan Yağcı <tyagci024@gmail.com>
2024-04-15 12:32:02 +0300

    gereksiz satır silmece

--- app/src/main/java/com/example/taskfour/viewModel/NewsListViewModel.kt
+++ app/src/main/java/com/example/taskfour/viewModel/NewsListViewModel.kt
@@ -5,23 +5,19 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.example.taskfour.model.NewsItem
 import com.example.taskfour.repository.TaskFourRepository
-import com.example.taskfour.service.NewsApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NewsListViewModel @Inject constructor(application: Application, private val repository: TaskFourRepository) : AndroidViewModel(application) {
 
     private val disposable = CompositeDisposable()
-    private val apiService = NewsApiService()
     val newsList = MutableLiveData<List<NewsItem>>()
     private val loading = MutableLiveData<Boolean>()
     val loadingObs: LiveData<Boolean>
