52b5e88c16c9db3759a2c90495c658eaa5783590
Turan Yağcı <tyagci024@gmail.com>
2024-04-20 13:30:25 +0300

    şema yükselttim

--- app/src/main/java/com/example/taskfour/room/CryptoDatabase.kt
+++ app/src/main/java/com/example/taskfour/room/CryptoDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.taskfour.model.CryptoModel
 
-@Database(entities = [CryptoModel::class], version = 7, exportSchema = false)
+@Database(entities = [CryptoModel::class], version = 8, exportSchema = false)
 abstract class CryptoDatabase : RoomDatabase() {
     abstract fun cryptoDao(): CryptoDao
 }
