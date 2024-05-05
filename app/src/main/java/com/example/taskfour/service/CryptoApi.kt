ea98bda0e072d09601cdf0dde72e36494dd88f66
Turan Yağcı <tyagci024@gmail.com>
2024-05-05 21:24:38 +0300

    hardcoded api class

--- app/src/main/java/com/example/taskfour/service/CryptoApi.kt
+++ app/src/main/java/com/example/taskfour/service/CryptoApi.kt
@@ -1,10 +1,15 @@
 package com.example.taskfour.service
 
 import com.example.taskfour.model.CryptoModel
+import com.example.taskfour.utilies.Constants
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CryptoApi {
 
-    @GET("coins/markets?vs_currency=usd&per_page=250&page=1")
+    @GET(Constants.END_POINT_COIN)
     suspend fun getList(): List<CryptoModel>
+
+    @GET("coins/markets?vs_currency=usd&per_page=250&page={page}")
+    suspend fun getListCoin(@Path("page") page: String): List<CryptoModel>
 }
