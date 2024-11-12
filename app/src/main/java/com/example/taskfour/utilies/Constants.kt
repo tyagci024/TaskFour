2ba66ab0e57414f9753e26c1dd907b625211587c
Turan Yağcı <122689077+tyagci024@users.noreply.github.com>
2024-11-12 18:43:16 +0300

    remove comment

--- app/src/main/java/com/example/taskfour/utilies/Constants.kt
+++ app/src/main/java/com/example/taskfour/utilies/Constants.kt
@@ -6,14 +6,5 @@ object Constants {
     const val BASE_URL_COIN_NEWS = "https://raw.githubusercontent.com/"
     const val END_POINT_NEWS = "tyagci024/pharmcy/main/news.json"
 }
-/*
-  "status": {
-    "error_code": 429,
-    "error_message": "You've exceeded the Rate Limit.
-    Please visit https://www.coingecko.com/en/api/pricing to
-     subscribe to our API plans for higher rate limits."
-  }
-}
-*/
 
-//bildirim
+//bildirim https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=2
